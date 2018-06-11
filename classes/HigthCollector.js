54cd7423fbd1ce595333103095f8576ab1d75e4e
Andrey Kernichniy <akernichniy@sinergo.ru>
2018-06-12 03:13:27 +0700

    add async

--- classes/HigthCollector.js
+++ classes/HigthCollector.js
@@ -2,7 +2,8 @@ import {
     HigthModel,
     CollectorsStatus
 } from '../models';
-import https from 'https';
+import axios from 'axios';
+import async from 'async';
 
 export class HigthCollector {
     constructor() {
@@ -32,6 +33,7 @@ export class HigthCollector {
         this._step = config.step;
         this._queryLimit = config.queryLimit;
         this._queryDelayMs = config.queryDelayMs;
+        
         return this._checkStatus()
             .then(() => this.startCollect())
             .catch(err => console.log(err));
@@ -52,33 +54,34 @@ export class HigthCollector {
 
     _checkStatus() {
         return new Promise((resolve, reject) => {
-            CollectorsStatus.find({}, (err, res) => {
-                err && reject(err);
-                if (res.length) {
-                    this._currentPoint.x = res[0].higthCollector.currentX;
-                    this._currentPoint.y = res[0].higthCollector.currentY;
-                    this._queryWasDone = res[0].higthCollector.queryWasDone;
-                    resolve();
-                } else {
-                    let status = new CollectorsStatus({
-                        higthCollector: {
-                            city: this.city,
-                            currentX: this._startPoint.x,
-                            currentY: this._startPoint.y,
-                            queryWasDone: 0,
-                            queryHaveErr: []
-                        }
-                    });
-                    status.save(err => {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            this._currentPoint = this._startPoint;
-                            resolve();
-                        }
-                    });
-                }
-            });
+            async.waterfall([
+                cb => CollectorsStatus.find({}, (err, res) => err ? cb(err) : cb(null, res)),
+                (res, cb) => {
+                    if (res.length) {
+                        this._currentPoint.x = res[0].higthCollector.currentX;
+                        this._currentPoint.y = res[0].higthCollector.currentY;
+                        this._queryWasDone = res[0].higthCollector.queryWasDone;
+                        cb(null);
+                    } else {
+                        let status = new CollectorsStatus({
+                            higthCollector: {
+                                city: this.city,
+                                currentX: this._startPoint.x,
+                                currentY: this._startPoint.y,
+                                queryWasDone: 0,
+                                queryHaveErr: []
+                            }
+                        });
+                        status.save(err => {
+                            if (err) {
+                                cb(err);
+                            } else {
+                                this._currentPoint = this._startPoint;
+                                cb(null);
+                            }
+                        });
+                    }
+                }], err =>  err ? reject(err) : resolve());
         });
     };
 
@@ -90,35 +93,36 @@ export class HigthCollector {
                 this.planNext();
             } else {
                 let url = this._createQuery();
-                https.get('url', res => {
-                    this._queryWasDone++;
-                    let answ = JSON.parse(res);
-                    if (answ.status === 'ok') {
-                        let dates = answ.results.map(point => new HigthModel({
-                            elevation: point.elevation,
-                            lat: point.location.lat,
-                            lng: point.location.lng,
-                            resolution: point.resolution
-                        }));
-                        HigthModel.insertMany(dates, err => console.log(err));
-                    } else {
-                        let findDoc = {
-                                city: this.city
-                            },
-                            updDoc = {
-                                $set: {
-                                    queryHaveErr: [{
-                                        url: url,
-                                        errMsg: answ.error_message
-                                    }]
-                                }
-                            };
-                        CollectorsStatus.findOneAndUpdate(findDoc, updDoc, (err, doc, res) => console.log(err));
+                async.waterfall([
+                    cb => axios.get(url).then(res => 
+                            res.data.status === 'OK' ? cb(null, res.data.results, null) : cb(null, null, res.data.error_message))
+                            .catch(err => cb(err)),
+                    (data, msg, cb) => {
+                        if (msg) {
+                            const findDoc = { city: this.city },
+                                updDoc = {
+                                    $set: {
+                                        queryHaveErr: [{
+                                            url: url,
+                                            errMsg: answ.error_message }]
+                                    }
+                                };
+                            CollectorsStatus.findOneAndUpdate(findDoc, updDoc, err => err ? cb(err) : cb(null));
+                        } else {
+                            console.time()
+                            let elv = data.map(point => new HigthModel({
+                                elevation: point.elevation,
+                                lat: point.location.lat,
+                                lng: point.location.lng,
+                                resolution: point.resolution
+                            }));
+                            console.timeEnd();
+                            HigthModel.insertMany(elv, err => err ? cb(err) : cb(null));
+                        }
                     }
-                })
+                ], err => err ? console.log(err) : null);
             }
         }, this._queryDelayMs);
-        return Promise.resolve();
     };
 
     stopCollect() {
